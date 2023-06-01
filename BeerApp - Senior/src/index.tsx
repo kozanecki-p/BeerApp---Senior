c17eac68596ed026384fce1faf939ddefe6a7c1e
Piotr Kozanecki <kozanecki.p@gmail.com>
2023-06-01 02:18:50 +0200

    Add basic PWA request caching

--- BeerApp - Senior/src/index.tsx
+++ BeerApp - Senior/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Router from './router';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@emotion/react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { theme } from './styles/theme';
 import './styles/global.css';
 
@@ -15,3 +16,8 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
